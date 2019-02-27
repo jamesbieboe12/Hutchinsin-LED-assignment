30345d4d7d8ff880ddf4478862dc9a4182e12b40
jamesbieboe12 <47893229+jamesbieboe12@users.noreply.github.com>
2019-02-27 14:32:44 -0800

    Update datmoneyhoney (1).c
    This is the updated version

--- datmoneyhoney (1).c
+++ datmoneyhoney (1).c
@@ -1,22 +1,29 @@
-# include <stdio.h>
-# define NUM_LEDS 135
-# define DATA_PIN 11
+#include <FastLED.h>
+#define NUM_LEDS 135
+#define DATA_PIN 11
+
 CRGB leds[NUM_LEDS];
+
 void setup(){
-    FastLED.addLeds<WS28128,DATA_PIN>[leds,NUM_LEDS];
+    FastLED.addLeds<WS2812B, DATA_PIN>(leds, NUM_LEDS);
 }
 void loop(){
     for(int i=0; i<NUM_LEDS; i++){
         leds[i] = CRGB :: Blue;
-        leds[(NUM_LEDS - i)] = CRGB :: yellow;
+        leds[(NUM_LEDS-i)] = CRGB :: yellow;
         FastLED.show();
         delay(30);
         leds[i] = CRGB :: Black;
-        leds[(Num_LEDS - i)] = CRGB :: Black
-        
-      leds[67] = CRGB::Green; 
-      FastLED.show();
-      delay(1000);
-        
+        leds[(NUM_LEDS - i)] = CRGB :: Black;
+        //blue and yellow light up and meet in the middle and the led lights green
+        leds[67] = CRGB::Green; 
+        FastLED.show();
+        delay(1000);
+        leds[i] = CRGB :: Green;
+        delay(100);
+        leds[(NUM_LEDS-i)] = CRGB :: Black;
+        //tells leds to go green then black
+        leds[i] = CRGB :: Red;
+        //the leds finishes off as a red light
     }
 }
