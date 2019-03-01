7b501a06dc0b88ac1b3760d3c4623edfd5383071
jamesbieboe12 <47893229+jamesbieboe12@users.noreply.github.com>
2019-03-01 09:06:21 -0500

    This is the updated version

--- datmoneyhoney (1).c
+++ datmoneyhoney (1).c
@@ -5,12 +5,12 @@
 CRGB leds[NUM_LEDS];
 
 void setup(){
-    FastLED.addLeds<WS2812B, DATA_PIN>(leds, NUM_LEDS);
+    FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
 }
 void loop(){
     for(int i=0; i<NUM_LEDS; i++){
         leds[i] = CRGB :: Blue;
-        leds[(NUM_LEDS-i)] = CRGB :: yellow;
+        leds[(NUM_LEDS-i)] = CRGB :: Yellow;
         FastLED.show();
         delay(30);
         leds[i] = CRGB :: Black;
@@ -18,7 +18,7 @@ void loop(){
         //blue and yellow light up and meet in the middle and the led lights green
         leds[67] = CRGB::Green; 
         FastLED.show();
-        delay(1000);
+        delay(500);
         leds[i] = CRGB :: Green;
         delay(100);
         leds[(NUM_LEDS-i)] = CRGB :: Black;
